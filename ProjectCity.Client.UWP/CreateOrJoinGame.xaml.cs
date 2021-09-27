a54a03a9a6deb7b553f1aefd4d637a82316185ab
TwentyCCent <75541353+TwentyCCent@users.noreply.github.com>
2021-09-27 19:28:22 +0200

    Page CreateOrJoinGame v001

--- ProjectCity.Client.UWP/CreateOrJoinGame.xaml.cs
+++ ProjectCity.Client.UWP/CreateOrJoinGame.xaml.cs
@@ -13,23 +13,23 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace ProjectCity.Client.UWP
 {
     /// <summary>
     /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class CreateOrJoinGame : Page
     {
-        public MainPage()
+        public CreateOrJoinGame()
         {
             this.InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            Frame.Navigate(typeof(CreateOrJoinGame));
+
         }
     }
 }
