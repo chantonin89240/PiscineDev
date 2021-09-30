82ae1c94c8ca81c1281cee0ceec9d0117575de20
Antonin CHATEIGNER <antonin.chateigner@diiage.org>
2021-09-30 16:57:12 +0200

    plateau de jeux

---
+++ ProjectCity.Client.UWP/Plate.xaml.cs
@@ -0,0 +1,44 @@
+﻿using ProjectCity.EntitiesShare;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ProjectCity.Client.UWP
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class Plate : Page
+    {
+        public Plate()
+        {
+            this.InitializeComponent();
+
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            /*Dictionary<string, object> Parameters = (Dictionary<string, object>)e.Parameter;
+            Company Company = (Company)Parameters["Compagny"];*/
+        }
+
+        // méthode qui incrémente les plateau (tour, argent, projet en cours, devops recruter)
+        public void UpdatePlate()
+        {
+
+        }
+    }
+}
--- ProjectCity.Server.Services/ServiceA.cs
+++ ProjectCity.Server.Services/ServiceA.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using ProjectCity.EntitiesShare;
+using ProjectCity.VM;
 
 namespace ProjectCity.Server.Services
 {
@@ -49,6 +50,7 @@ namespace ProjectCity.Server.Services
 
                 }
             );
+            List<Training> listeForma = Call.FromJson<List<Training>>("formation");
 
             return listeTraining;
         }
