b791bf3232c4ee071ebe2d062fd13ed302dcc735
Antonin CHATEIGNER <antonin.chateigner@diiage.org>
2021-10-05 11:05:16 +0200

    maj

--- ProjectCity.VM/Serializer.cs
+++ ProjectCity.VM/Serializer.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
-using Windows.Storage;
 using FileAttributes = System.IO.FileAttributes;
 
 namespace ProjectCity.VM
